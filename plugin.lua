a3622804390eaf849beaf72b7dcfbc167d48e5bd
Skyline777HD <skyline777.minecraft@gmail.com>
2015-12-19 03:13:23 +0100

    Check if there is empty space to put the chest. If the player inventory is empty don't create a chest.

--- plugin.lua
+++ plugin.lua
@@ -5,10 +5,8 @@
 
 -- Deathdrop plugin allows for a players inventory to be copied to a chest before death to remove some of the pain of long mining trips.
 
-  --To do: add functionality to make sure that the chest is not placed in lava/bedrock basically anywhere that would reneder the use of the plugin u and to check if
-    --the chest has enough slots to hold complete inventory.
-
---TODO comprobar si un cofre esta cerca, hacer que el cofre no se cree si el inventario esta vacio, hacer que el cofre no haga drop de cofre.
+  -- To do: add functionality to make sure that the chest is not placed in lava/bedrock basically anywhere that would reneder the use of the plugin u and to check if
+    -- the chest has enough slots to hold complete inventory.
 
 
 function Initialize(Plugin)
@@ -16,45 +14,68 @@ function Initialize(Plugin)
 	Plugin:SetVersion(.1)
 
 	--Hook into the HOOK_KILLING to see when something is going to die
-  cPluginManager.AddHook(cPluginManager.HOOK_KILLING, OnKilling)
-  -- let the user know our plugin is running
+    cPluginManager:AddHook(cPluginManager.HOOK_KILLING, OnKilling)
+    -- let the user know our plugin is running
 	LOG("Initialized " .. Plugin:GetName() .. " v." .. Plugin:GetVersion())
-  -- let upstream know we started ok....not that we have a fail mode yet...
+    -- let upstream know we started ok....not that we have a fail mode yet...
 	return true
 end
 
 function OnKilling(Victim, Killer, TDI)
-  --In this function something is about to die and we need to figure out if its a player.
+  -- In this function something is about to die and we need to figure out if its a player.
   if (Victim:IsPlayer()) then
-    --Ok so it a player 
-    --Give a lighting bolt above to let the player know their stuff is saved
-    Victim:GetWorld():CastThunderbolt(Victim:GetPosition().x, Victim:GetPosition().y, Victim:GetPosition().z);
-    --Build the chest and face no sure what ZM is yet...
-    Victim:GetWorld():SetBlock(Victim:GetPosition().x,(Victim:GetPosition().y +2) , Victim:GetPosition().z, E_BLOCK_CHEST, E_META_CHEST_FACING_ZM)
-    Victim:GetWorld():SetBlock(Victim:GetPosition().x +1,(Victim:GetPosition().y +2) , Victim:GetPosition().z, E_BLOCK_CHEST, E_META_CHEST_FACING_ZM)
-    --get the players items and copy them to a chest
-    local Items = cItems()
-    local TESTVAR = Victim:GetInventory()
-    TESTVAR:CopyToItems(Items)
-    Victim:GetWorld():DoWithChestAt(
-		Victim:GetPosition().x, (Victim:GetPosition().y +2), Victim:GetPosition().z,
-		function(a_Chest)
-			a_Chest:GetContents():AddItems(Items)
+    -- Ok so it is a player
+    -- get the players items and copy them to a chest
+    local Items    = cItems()
+    local TESTVAR  = Victim:GetInventory()
+	TESTVAR:CopyToItems(Items)
+	-- get the diferent grids from the inventory to check if they are empty (if -1 they are empty)
+	local InventoryGrid = Victim:GetInventory():GetInventoryGrid():GetLastUsedSlot()
+	local HotbarGrid    = Victim:GetInventory():GetHotbarGrid():GetLastUsedSlot()
+	local ArmorGrid     = Victim:GetInventory():GetArmorGrid():GetLastUsedSlot()
+	-- Give a lighting bolt above to let the player know their stuff is saved
+	Victim:GetWorld():CastThunderbolt(Victim:GetPosition().x, Victim:GetPosition().y, Victim:GetPosition().z);
+	-- Check the sum of the grids
+	if (InventoryGrid + HotbarGrid + ArmorGrid ~= -3) then
+		-- Build the chest and check if the block is not air
+		Victim:GetWorld():SetBlock(Victim:GetPosition().x, Victim:GetPosition().y +1, Victim:GetPosition().z, E_BLOCK_CHEST, E_META_CHEST_FACING_ZM)
+		if (Victim:GetWorld():GetBlock(Victim:GetPosition().x +1,Victim:GetPosition().y +1, Victim:GetPosition().z) == E_BLOCK_AIR) then
+			Victim:GetWorld():SetBlock(Victim:GetPosition().x +1,Victim:GetPosition().y +1, Victim:GetPosition().z, E_BLOCK_CHEST, E_META_CHEST_FACING_ZM)
+		else
+			Victim:GetWorld():SetBlock(Victim:GetPosition().x,Victim:GetPosition().y , Victim:GetPosition().z, E_BLOCK_CHEST, E_META_CHEST_FACING_ZM)
 		end
-	)
-	Victim:GetWorld():DoWithChestAt(
-		Victim:GetPosition().x +1, (Victim:GetPosition().y +2), Victim:GetPosition().z,
-		function(a_Chest)
-			a_Chest:GetContents():AddItems(Items)
+		-- copy the items in the chest
+		Victim:GetWorld():DoWithChestAt(
+			Victim:GetPosition().x, (Victim:GetPosition().y +1), Victim:GetPosition().z,
+			function(a_Chest)
+				a_Chest:GetContents():AddItems(Items)
+			end
+		)
+		if (Victim:GetWorld():GetBlock(Victim:GetPosition().x +1, Victim:GetPosition().y +1, Victim:GetPosition().z) == E_BLOCK_AIR) then
+			Victim:GetWorld():DoWithChestAt(
+				Victim:GetPosition().x +1, Victim:GetPosition().y +1, Victim:GetPosition().z,
+				function(a_Chest)
+					a_Chest:GetContents():AddItems(Items)
+				end
+			)
+		else
+			Victim:GetWorld():DoWithChestAt(
+				Victim:GetPosition().x, Victim:GetPosition().y, Victim:GetPosition().z,
+				function(a_Chest)
+					a_Chest:GetContents():AddItems(Items)
+				end
+			)
 		end
-	)
-    --Remove the items from the inventory so that when the player dies the system doesnt drop
-    Victim:GetInventory():Clear()      
-     
+		-- Remove the items from the inventory so that when the player dies the system doesnt drop
+		Victim:GetInventory():Clear()
+		Victim:SendMessage(cChatColor.Yellow .. "[INFO] " .. cChatColor.White .. " Your items are stored in a chest")
+	else
+		Victim:SendMessage(cChatColor.Yellow .. "[INFO] " .. cChatColor.White .. " You don't have items in the inventory")
+	end
   else
-      --This is a Mob which this plugin does not care about.
+      -- This is a Mob which this plugin does not care about.
   end
-  --return false to let everyone know we good...I think...readup on this
+  -- return false to let everyone know we good...I think...readup on this
   return false
-  --close up the program.
+  -- close up the program.
 end
